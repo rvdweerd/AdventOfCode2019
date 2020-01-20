29c2d5ee9cda35574e4dd10d63f0ac09319aa92f
RogerVDW_VS2019 <rogerscode@gmail.com>
2020-01-20 20:11:27 +0100

    Day23 part 2 completed

--- AdventOfCode2019/Source.cpp
+++ AdventOfCode2019/Source.cpp
@@ -55,6 +55,96 @@ void Day23a()
 		}
 		if (CeaseOps) break;
 	}
+	std::cout << "\nPress [Enter] to continue"; std::cin.get();
+	return;
+}
+void Day23b()
+{
+	// Initialize the computer network
+	std::map<int, IntCode_net*> compNet;
+	static constexpr int networkSize = 50;
+	for (int i = 0; i < networkSize; i++)
+	{
+		compNet.emplace(i, new IntCode_net("Resources/day23input.txt"));
+		std::vector<long long int> retVec = compNet[i]->Run({ i });
+	}
+
+	// Run
+	// Principle: loop the computers to pass -1 values. When a response is received, put these on a global queue
+	// Handle queue responses (and add any subsequent responses to the queue) until the queue is empty,
+	// then go back to looping with -1 values.
+	std::queue<long long int> queue;
+	bool CeaseOps = false;
+	int instructionCount = 0;
+	int idleCount = 0;
+	std::vector<long long int> retVec;
+	struct NAT_mem
+	{
+		long long int X=0;
+		long long int Y=0;
+		long long int X_lastsent = 0;
+		long long int Y_lastsent = 0;
+	} NAT;
+	
+
+	while (!CeaseOps)
+	{
+		for (int i = 0; i < networkSize; i++) // loop the network for responses
+		{
+			{
+				std::cout << "Passing to comp: " << i << ", instruction X=" << -1 << '\n';
+				std::cout << "Receiving back : ";
+				retVec = compNet[i]->Run({ -1 });
+				instructionCount++;
+				retVec.size() == 0 ? idleCount++ : idleCount = 0;
+				if (idleCount == networkSize) // Network is idle
+				{
+					queue.push({ 0 }); queue.push({ NAT.X }); queue.push({ NAT.Y });
+					if (NAT.Y == NAT.Y_lastsent)
+					{
+						std::cout << "The NAT has sent the instruction Y=" << NAT.Y << " twice in a row after " << instructionCount << " instructions. END.";
+						CeaseOps = true;
+						retVec.clear();
+						while (!queue.empty()) queue.pop();
+					}
+					else
+					{
+						NAT.X_lastsent = NAT.X;
+						NAT.Y_lastsent = NAT.Y;
+					}
+				}
+				for (auto v : retVec) queue.push(v);
+				retVec.clear();
+			}
+
+			while (!queue.empty())
+			{
+				int compId = (int)queue.front(); queue.pop(); 
+				long long int instr1 = queue.front(); queue.pop();
+				long long int instr2 = queue.front(); queue.pop();
+				if (compId == 255)
+				{
+					std::cout << "Packet sent to address 255. instruction X="<<instr1<<", Y=" << instr2<<'\n';
+					NAT.X = instr1;
+					NAT.Y = instr2;
+					//std::cout << "Receiving back : ";
+					//retVec = compNet[compId]->Run({ instr1, instr2 });
+				}
+				else
+				{
+					std::cout << "Passing to comp: " << compId << ", instruction X=" << instr1 << ", Y=" << instr2 << '\n';
+					std::cout << "Receiving back : ";
+					retVec = compNet[compId]->Run({ instr1, instr2 });
+					instructionCount++;
+					retVec.size() == 0 ? idleCount++ : idleCount = 0;
+				}
+				for (auto v : retVec) queue.push(v);
+				retVec.clear();
+			}
+		}
+		if (CeaseOps) break;
+	}
+	return;
 }
 
 int main()
@@ -68,6 +158,7 @@ int main()
 	//Day22a_clean1();
 	//Day22a_clean2();
 	Day23a();
+	Day23b();
 	
 	while (!_kbhit());
 	return 0;
